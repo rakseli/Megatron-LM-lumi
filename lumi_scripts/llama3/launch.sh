6ed5f99c8cf1097af10c8303859ed455dfa9eab7
Akseli Reunamo <akseli.y.reunamo@utu.fi>
2024-10-18 11:37:17 +0300

    unify with EasyBuild conda-distribured

--- lumi_scripts/llama3/launch.sh
+++ lumi_scripts/llama3/launch.sh
@@ -1,12 +1,36 @@
-#!/bin/bash
+#!/bin/bash -e
+
+
+# MIOPEN needs some initialisation for the cache as the default location
+# does not work on LUMI as Lustre does not provide the necessary features.
+export MIOPEN_USER_DB_PATH="/tmp/$(whoami)-miopen-cache-$SLURM_NODEID"
+export MIOPEN_CUSTOM_CACHE_DIR=$MIOPEN_USER_DB_PATH
+
+# Set MIOpen cache to a temporary folder.
+if [ $SLURM_LOCALID -eq 0 ] ; then
+    rm -rf $MIOPEN_USER_DB_PATH
+    mkdir -p $MIOPEN_USER_DB_PATH
+fi
+sleep 2
+
+# Set interfaces to be used by RCCL.
+# This is needed as otherwise RCCL tries to use a network interface it has
+# noa ccess to on LUMI.
+export NCCL_SOCKET_IFNAME=hsn0,hsn1,hsn2,hsn3
+export NCCL_NET_GDR_LEVEL=3
+
+
+# The usual PyTorch initialisations (also needed on NVIDIA)
+# Note that since we fix the port ID it is not possible to run, e.g., two
+# instances via this script using half a node each.
+export MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_PORT=9999
 export WORLD_SIZE=$SLURM_NTASKS
 export RANK=$SLURM_PROCID
 export LOCAL_RANK=$SLURM_LOCALID
-
-
-echo "Run on $SLURMD_NODENAME ($SLURM_PROCID/$WORLD_SIZE)," \
-     "master $MASTER_ADDR port $MASTER_PORT," \
-     "GPUs $SLURM_GPUS_ON_NODE"
-
-
-python3 "$@"
+export FI_CXI_DEFAULT_CQ_SIZE=262144
+export RCCL_KERNEL_COLL_TRACE_ENABLE=1 
+export PYTORCH_HIP_ALLOC_CONF='max_split_size_mb:512'
+export HSA_FORCE_FINE_GRAIN_PCIE=1
+# Run application
+python "$@"
